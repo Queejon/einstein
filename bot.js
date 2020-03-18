315dbec66dea173f49819b5bc400021c6b39895a
Queejon <queejonjin@gmail.com>
2020-03-18 10:24:20 -0500

    Logging Changes

--- bot.js
+++ bot.js
@@ -10,11 +10,22 @@ client.on('ready', () => {
 });
 
 client.on('message', (msg) => {
-
+    const message = msgprecon(msg);
+    if(message == null) return;
+    if(message == 'test'){
+        //msg.reply(`\`\`\`I here you loud and clear!\`\`\``);
+    }
+    else if(message.substring(0, message.indexOf(' ')).toLowerCase() == 'roll'){
+        roll(msg, message.substring(5, message.indexOf('d')), message.substring(message.indexOf('d')+1));
+    }
+    else if(message.toLowerCase() == 'roll'){
+        msg.reply(`you need to specify a number of dice and sides (i.e. 1d6).`);
+    }
 });
 
 client.on('messageDelete', (msg) => {
     const channel = msg.guild.channels.cache.get(config.logging_channel);
+    if(!loggingChannelBlacklist(msg)) return;
     channel.send(`**DELETED:**
     Message by \`${msg.author.username}#${msg.author.discriminator}\` in <#${msg.channel.id}>:
     \`${msg.content}\`
@@ -23,9 +34,73 @@ client.on('messageDelete', (msg) => {
 
 client.on('messageUpdate', (msg) => {
     const channel = msg.guild.channels.cache.get(config.logging_channel);
+    if(!loggingChannelBlacklist(msg)) return;
     channel.send(`**EDITED:**
     Message by \`${msg.author.username}#${msg.author.discriminator}\` in <#${msg.channel.id}>:
     was: \`${msg.content}\`
     now: \`${msg.channel.messages.cache.get(msg.id).content}\`
     `)
 });
+
+function loggingChannelBlacklist(msg){
+    for(let k = 0; k < config.logging_channel_blacklist.length; k++){
+        if(msg.channel.id == config.logging_blacklist[k])
+            return true;
+    }
+    return false;
+}
+
+function loggingUserBlacklist(msg){
+    for(let k = 0; k < config.logging_user_blacklist.length; k++){
+        if(msg.channel.id == config.logging_blacklist[k])
+            return true;
+    }
+    return false;
+}
+
+function msgprecon(msg){
+    msg.content = msg.content.trim();
+    if(msg.content.charAt(0) != '$') return null;
+        return msg.content.substring(1);
+}
+
+function roll(msg, numofdice, numofsides){
+    //console.log(`Number of dice: ${numofdice}`);
+    //console.log(`Number of sides: ${numofsides}`);
+    numofdice = new Number(numofdice);
+    numofsides = new Number(numofsides);
+    var result = 0;
+    try{
+        var resultarr = new Array(numofdice);
+    }
+    catch{
+        msg.channel.send('ERR too many dice!');
+        return;
+    }
+    try{
+        for(let roll = 1; roll <= numofdice; roll++){
+          resultarr[roll] = randomnum(1, numofsides);
+          //console.log(resultarr[roll]);
+          result += resultarr[roll];
+        }
+    }
+    catch{
+        msg.channel.send('ERR too many sides!');
+        return;
+    }
+    result = 'Total: ' + result + '     Rolls: '
+    for(let roll = 1; roll <= numofdice; roll++){
+        if(roll != numofdice){
+            result = result + resultarr[roll] + ', ';
+        }
+        else{
+            result = result + resultarr[roll];
+        }
+    }
+    //console.log(result);
+    msg.channel.send(result.toString());
+}
+
+function randomnum(min, max){
+    return Math.floor(Math.random()*(max-min+1))+min;
+}
