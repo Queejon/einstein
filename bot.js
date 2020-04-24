7aa7ced4aaa198fc87055a9f9d557e028974f3be
Queejon <queejonjin@gmail.com>
2020-04-23 23:40:59 -0500

    Logging, Langauge, and Join/Leave

--- bot.js
+++ bot.js
@@ -9,37 +9,133 @@ client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
+client.on('guildMemberAdd', (mem) => {
+    const channel = client.guilds.cache.get('698279564555583562').channels.cache.get('698279567885991979');
+    channel.send(`
+    Welcome <@${mem.user.id}>! Please tap/click on this --> <#698280079733555230> <-- for help on using Discord and the servers rules.
+    For any further help please contact Jonah.
+    `);
+    mem.roles.add('702297377436074047');
+    const logging_channel = msg.guild.channels.cache.get(config.logging_channel);
+    channel.send(`
+    **JOINED:**
+        *User:* \`${mem.user.id}\`
+        *Account Age:* \`${new Date(mem.user.createdTimestamp).toLocaleString()} ${((new Date()).toString().split('(')[1] || "").slice(0, -1)}\`
+    `);
+});
+
+client.on('guildMemberRemove', (mem) => {
+    const channel = client.guilds.cache.get('698279564555583562').channels.cache.get('698279567885991979');
+    channel.send(`
+    Goodbye <@${mem.user.id}>!
+    `);
+
+    const logging_channel = msg.guild.channels.cache.get(config.logging_channel);
+    channel.send(`
+    **LEFT:**
+        *User:* \`${mem.user.id}\`
+        *Account Age:* \`${new Date(mem.user.createdTimestamp).toLocaleString()} ${((new Date()).toString().split('(')[1] || "").slice(0, -1)}\`
+    `);
+});
+
 client.on('message', (msg) => {
-    const message = msgprecon(msg);
-    if(message == null) return;
-    if(message == 'test'){
-        //msg.reply(`\`\`\`I here you loud and clear!\`\`\``);
-    }
-    else if(message.substring(0, message.indexOf(' ')).toLowerCase() == 'roll'){
-        roll(msg, message.substring(5, message.indexOf('d')), message.substring(message.indexOf('d')+1));
-    }
-    else if(message.toLowerCase() == 'roll'){
-        msg.reply(`you need to specify a number of dice and sides (i.e. 1d6).`);
-    }
+    if(msg.author.id != "689553615068594277")
+        if(!languageFilter(msg)){
+            const message = msgprecon(msg);
+            if(message == null) return;
+            if(message == 'test'){
+                //msg.reply(`\`\`\`I here you loud and clear!\`\`\``);
+            }
+            else if(message.substring(0, message.indexOf(' ')).toLowerCase() == 'roll'){
+                roll(msg, message.substring(5, message.indexOf('d')), message.substring(message.indexOf('d')+1));
+            }
+            else if(message.toLowerCase() == 'roll'){
+                msg.reply(`you need to specify a number of dice and sides (i.e. 1d6).`);
+            }
+        }
+        else{
+            if(msg.deletable)
+                msg.delete();
+            msg.channel.send(`Message removed for foul language.`);
+        }
 });
 
 client.on('messageDelete', (msg) => {
-    const channel = msg.guild.channels.cache.get(config.logging_channel);
-    if(loggingChannelBlacklist(msg)) return;
-    if(loggingUserBlacklist(msg)) return;
-    channel.send(`**DELETED:**
-    *Author:* \`${msg.author.username}#${msg.author.discriminator}\` 
-    *Channel:* <#${msg.channel.id}> 
-    *Creation Timestamp:* \`${new Date(msg.createdTimestamp).toLocaleString()} ${((new Date()).toString().split('(')[1] || "").slice(0, -1)}\`
-    *Message:*
+    const backup_channel = msg.guild.channels.cache.get(config.logging_channel_backup);
+    if(!languageFilter(msg)){
+        backup_channel.send(`**DELETED:**
+        *Author:* \`${msg.author.username}#${msg.author.discriminator}\` 
+        *Channel:* <#${msg.channel.id}> 
+        *Creation Timestamp:* \`${new Date(msg.createdTimestamp).toLocaleString()} ${((new Date()).toString().split('(')[1] || "").slice(0, -1)}\`
+        *Message:*
+        
+        /------------------------------------/
+        ${msg.content}
+        /------------------------------------/
+        `);
+
+        const channel = msg.guild.channels.cache.get(config.logging_channel);
+        if(loggingChannelBlacklist(msg)) return;
+        if(loggingUserBlacklist(msg)) return;
+        channel.send(`**DELETED:**
+        *Author:* \`${msg.author.username}#${msg.author.discriminator}\` 
+        *Channel:* <#${msg.channel.id}> 
+        *Creation Timestamp:* \`${new Date(msg.createdTimestamp).toLocaleString()} ${((new Date()).toString().split('(')[1] || "").slice(0, -1)}\`
+        *Message:*
+        
+        /------------------------------------/
+        ${msg.content}
+        /------------------------------------/
+        `);
+    }
+    else{
+        backup_channel.send(`**REMOVED(FOUL_LANGUAGE):**
+        *Author:* \`${msg.author.username}#${msg.author.discriminator}\` 
+        *Channel:* <#${msg.channel.id}> 
+        *Creation Timestamp:* \`${new Date(msg.createdTimestamp).toLocaleString()} ${((new Date()).toString().split('(')[1] || "").slice(0, -1)}\`
+        *Message:*
+        
+        /------------------------------------/
+        ${msg.content}
+        /------------------------------------/
+        `);
+
+        const channel = msg.guild.channels.cache.get(config.logging_channel);
+        if(loggingChannelBlacklist(msg)) return;
+        if(loggingUserBlacklist(msg)) return;
+        channel.send(`**REMOVED(FOUL_LANGUAGE):**
+        *Author:* \`${msg.author.username}#${msg.author.discriminator}\` 
+        *Channel:* <#${msg.channel.id}> 
+        *Creation Timestamp:* \`${new Date(msg.createdTimestamp).toLocaleString()} ${((new Date()).toString().split('(')[1] || "").slice(0, -1)}\`
+        *Message:*
+        
+        /------------------------------------/
+        ${msg.content}
+        /------------------------------------/
+        `);
+    }
+});
+
+client.on('messageUpdate', (msg) => {
+    const backup_channel = msg.guild.channels.cache.get(config.logging_channel_backup);
+    backup_channel.send(`**EDITED:**
+    Message by \`${msg.author.username}#${msg.author.discriminator}\` in <#${msg.channel.id}>:
+    
+    
+    *Was:* 
     
     /------------------------------------/
     ${msg.content}
     /------------------------------------/
+    
+    
+    *Now:* 
+    
+    /------------------------------------/
+    ${msg.channel.messages.cache.get(msg.id).content}
+    /------------------------------------/
     `);
-});
 
-client.on('messageUpdate', (msg) => {
     const channel = msg.guild.channels.cache.get(config.logging_channel);
     if(loggingChannelBlacklist(msg)) return;
     if(loggingUserBlacklist(msg)) return;
@@ -59,7 +155,7 @@ client.on('messageUpdate', (msg) => {
     /------------------------------------/
     ${msg.channel.messages.cache.get(msg.id).content}
     /------------------------------------/
-    `)
+    `);
 });
 
 function loggingChannelBlacklist(msg){
@@ -82,6 +178,14 @@ function msgprecon(msg){
     msg.content = msg.content.trim();
     if(msg.content.charAt(0) != '$') return null;
         return msg.content.substring(1);
+}
+
+function languageFilter(msg){
+    msg.content = msg.content.toLowerCase();
+    for(let k = 0; k < config.banned_words.length; k++)
+        if(msg.content.indexOf(config.banned_words[k]) >= 0)
+            return true;
+    return false;
 }
 
 function roll(msg, numofdice, numofsides){
